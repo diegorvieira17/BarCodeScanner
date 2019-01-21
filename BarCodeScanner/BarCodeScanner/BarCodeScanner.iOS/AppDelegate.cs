d2af03cfa52fe833788a89f9ecbfdd93a50affa5
Diego Vieira <diegorvieira17@outlook.com>
2019-01-21 17:22:16 -0200

    Corrigido erro do AppDelegade que não estava reconhecendo a classe .Net do ZXing. Para isso foi atualizado o pacote ZXing.Net.Mobile e Adicionado o pacote ZXing.Net.Mobile.Forms em todos os projetos.

--- BarCodeScanner/BarCodeScanner/BarCodeScanner.iOS/AppDelegate.cs
+++ BarCodeScanner/BarCodeScanner/BarCodeScanner.iOS/AppDelegate.cs
@@ -23,7 +23,7 @@ namespace BarCodeScanner.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            global::ZXing.Net.Mobile.Forms.iOS.Plataform.Init();
+            global::ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
